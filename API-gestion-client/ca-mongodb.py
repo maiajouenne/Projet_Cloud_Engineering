b6f33f9c67c8301e747bf20ada862a671fbd585e
Gianluca <gianlucaschileo@protonmail.com>
2023-12-12 21:59:27 +0100

    update ca

--- API-gestion-client/ca-mongodb.py
+++ API-gestion-client/ca-mongodb.py
@@ -4,7 +4,7 @@ import os
 
 app = FastAPI()
 
-host_url = os.getenv("HOST_URL", "http://127.0.0.1:8000")  
+host_url = os.getenv("HOST_URL", "http://127.0.0.1:50127")  
 
 @app.get("/data")
 async def get_data():
@@ -16,14 +16,10 @@ async def get_data():
     
     ca_dict = {}
     for ticket in data:
-        articles = ticket["articles"]
-        ca = 0
-        for article in articles:
-            ca += article[1] * article[2]
         if ticket["magasin"] in ca_dict:
-            ca_dict[ticket["magasin"]] += ca
+            ca_dict[ticket["magasin"]] += ticket["total"]
         else:
-            ca_dict[ticket["magasin"]] = ca
+            ca_dict[ticket["magasin"]] = ticket["total"]
 
-    return HTTPResponse(str(ca_dict), content_type="application/json")
+    return ca_dict
 
